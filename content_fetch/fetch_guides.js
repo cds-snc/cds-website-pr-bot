9be25185e45be404347e639ade72058be3528cdf
omartehsin1 <>
2022-02-01 16:51:33 -0500

    added new fetch calls, updated index files

---
+++ content_fetch/fetch_guides.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+const buildFileName = require("../utils/buildFileName");
+
+var getGuides = async function(lang) {
+    return await fetch(process.env.STRAPI_ENDPOINT + "guides-" + lang + "s")
+    .then(response => response.json())
+    .then( 
+       data => {
+           let files = [];
+           for (p in data) {
+               let post = data[p]
+               let out = "";
+               out += "---\n";
+               out += "title: '" + post.Title + "'\n";
+               out += "translationKey: " + post.TranslationID + "\n";
+               out += "description: >-\n";
+               out += "  " + post.Description + "\n";
+               out += "buttonText: " + postButtonText + "\n"
+               out += "buttonAria: " + post.ButtonAria + "\n"
+               out += "weight: " + post.Weight + "\n"
+               out += "url: " + post.Url + "\n"
+
+               let slug = buildFileName(post.Title);
+               files.push({body: out, fileName: slug + ".md"})
+           }
+           return files
+       } 
+    )
+}
+module.exports = getGuides
