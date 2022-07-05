4881376d2da625ac3d2e4a5e786e5d0bfb2618d8
omartehsin1 <>
2022-07-05 12:45:26 -0400

    reverted articles change

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const getProducts = require("./content_fetch/fetch_products");
 const getProductSuite = require("./content_fetch/fetch_product_suite");
 const getGuides = require("./content_fetch/fetch_guides");
 
-const getArticlesPost = require("./content_fetch/fetch_articles_post")
 
 
 async function closePRs() {
@@ -180,7 +179,6 @@ async function run() {
   // Team Members
   var teamMembersNew = await getTeamMembers();
 
-  var articlesPostsNew = await getArticlesPost();
 
   // Products
   // endpoints are: "products-partnerships-lang", "products-platform-lang", "products-resources-lang"
@@ -219,7 +217,6 @@ async function run() {
   // Blog posts
   await createAndUpdateFiles(blogPostsEnNew, existingContentEN.data.tree, "en", "blog/posts/", branchName);
   await createAndUpdateFiles(blogPostsFrNew, existingContentFR.data.tree, "fr", "blog/posts/", branchName);
-  await createAndUpdateFiles(articlesPostsNew, existingContentEN.data.tree, "en", "artcles/posts/", branchName);
   // Job Postings
   await createAndUpdateFiles(jobPostsEnNew, existingContentEN.data.tree, "en", "careers/positions/", branchName);
   await createAndUpdateFiles(jobPostsFrNew, existingContentFR.data.tree, "fr", "careers/positions/", branchName);
