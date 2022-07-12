5a801c38faf8d52091f83d45de48ded75c4a44d0
omartehsin1 <>
2022-07-12 15:01:30 -0400

    fixing mistake from previous branch

--- content_fetch/fetch_blog_posts.js
+++ content_fetch/fetch_blog_posts.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const buildFileName = require("../utils/buildFileName");
 
 var getBlogPosts = async function(lang) {
-  return await fetch(process.env.STRAPI_ENDPOINT + "blog-" + lang + "s")
+  return await fetch(process.env.GC_ARTICLES_BLOG_ENDPOINT)
   .then(response => response.json())
   .then(
     data => {
@@ -13,31 +13,35 @@ var getBlogPosts = async function(lang) {
         let out = "";
         out += "---\n";
         out += "layout: blog\n";
-        out += "title: '" + post.Title + "'\n";
+        out += "title: '" + post.title.rendered + "'\n";
         out += "description: >-\n";
-        out += "  " + post.Description + "\n";
-        out += "author: '" + post.AuthorAndTitle + "'\n";
-        out += "date: '" + post.PublishDate + "'\n";
+        out += "  " + post.markdown.excerpt.rendered + "\n";
+        out += "author: '" + post.meta.gc_author_name + "'\n";
+        out += "date: '" + post.date + "'\n";
+
         // Strapi can only interface with the S3 URLS for the images, so we need to convert them to
         // Cloudfront so they will be externally visible
-        out += "image: " + post.BannerImage.url.replace(
+        out += "image: " + post.yoast_head_json.og_image[0].url.replace(
           "https://cds-website-assets-prod.s3.ca-central-1.amazonaws.com",
           "https://de2an9clyit2x.cloudfront.net") + "\n";
-        out += "image-alt: " + post.ImageAltText + "\n";
-        out += "thumb: " + post.BannerImage.formats.small.url.replace(
+        out += "image-alt: " + post._embedded['wp:featuredmedia'][0].caption.rendered + "\n";
+        out += "thumb: " + post._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url.replace(
           "https://cds-website-assets-prod.s3.ca-central-1.amazonaws.com",
           "https://de2an9clyit2x.cloudfront.net") + "\n";
-        out += "translationKey: " + post.TranslationID + "\n";
+        out += "translationKey: " + post.slug + "\n";
         out += "---\n";
+
         // Convert any body image URLS to Cloudfront
-        while (post.Body.indexOf("https://cds-website-assets-prod.s3.ca-central-1.amazonaws.com") !== -1) {
-          post.Body = post.Body.replace(
+        while (post.markdown.content.rendered.indexOf("https://cds-website-assets-prod.s3.ca-central-1.amazonaws.com") !== -1) {
+          post.markdown.content.rendered = post.markdown.content.rendered.replace(
             "https://cds-website-assets-prod.s3.ca-central-1.amazonaws.com",
             "https://de2an9clyit2x.cloudfront.net");
         }
-        out += post.Body + "\n";
+
+        out += post.markdown.content.rendered + "\n";
         
-        let slug = buildFileName(post.Title);
+        let slug = buildFileName(post.title.rendered);
+
         files.push({body: out, fileName: slug + ".md"})
       }
       
@@ -45,4 +49,5 @@ var getBlogPosts = async function(lang) {
     }
   )
 }
+
 module.exports = getBlogPosts;
