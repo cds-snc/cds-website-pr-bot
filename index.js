c65c76715b65dfa027723b65ae6054951b528a12
omartehsin1 <>
2022-09-27 13:32:18 -0400

    guides test

--- index.js
+++ index.js
@@ -112,6 +112,44 @@ const createAndUpdateFiles = async (newFiles, oldFiles, lang, subpath, branchNam
   }
 }
 
+const createAndUpdateGuidesFiles = async (newFiles, oldFiles, lang, subpath, branchName) => {
+  let path = "content/" + lang + "/";
+  for (f in newFiles) {
+    var exists = (oldFiles.name && oldFiles.name == newFiles[f].fileName) ? [oldFiles] : oldFiles.filter(oldFile => oldFile.path == subpath + newFiles[f].fileName);
+    let content = Base64.encode(newFiles[f].body)
+    if (exists.length == 0) {
+      // Create new File
+      await octokit.repos.createOrUpdateFileContents({
+        owner: 'cds-snc',
+        repo: 'digital-canada-ca',
+        path: path + subpath + newFiles[f].fileName,
+        content: content,
+        branch: `guides-${branchName}`,
+        message: "Added new file: " + newFiles[f].fileName
+      })
+    } else {
+      await octokit.repos.getContent({
+        owner: 'cds-snc',
+        repo: 'digital-canada-ca',
+        path: path + exists[0].path
+      }).then(async result => {
+        if (Base64.decode(result.data.content) != newFiles[f].body) {
+          // Update existing file
+          await octokit.repos.createOrUpdateFileContents({
+            owner: 'cds-snc',
+            repo: 'digital-canada-ca',
+            sha: exists[0].sha, // if update this is required
+            path: path + exists[0].path,
+            content: content,
+            branch: `guides-${branchName}`,
+            message: "Updated file: " + newFiles[f].fileName
+          })
+        }
+      });
+    }
+  }
+}
+
 const updateTeamFile = async (newFile, branchName) => {
 
   let content = Base64.encode(newFile[0].body)
@@ -145,6 +183,68 @@ Steps to update:
 4. Do a pull request onto digital-canada-ca with the new content
 
 */
+
+async function runGuides() {
+  let treeShas = await octokit.repos.getContent({
+    owner: 'cds-snc',
+    repo: 'digital-canada-ca',
+    path: "/content",
+  });
+  
+  let existingContentEN = await octokit.git.getTree({
+    owner: 'cds-snc',
+    repo: 'digital-canada-ca',
+    tree_sha: treeShas.data.filter(tree => tree.name === "en")[0].sha, // filter by name in case this directory is ever modified / added to
+    recursive: true
+  });
+  let existingContentFR = await octokit.git.getTree({
+    owner: 'cds-snc',
+    repo: 'digital-canada-ca',
+    tree_sha: treeShas.data.filter(tree => tree.name === "fr")[0].sha,
+    recursive: true
+  });
+
+  var gcArticlesGuidesEn = await getGuidesFromGCArticles("en");
+  var gcArticlesGuidesFr = await getGuidesFromGCArticles("fr");
+
+  branchName = `content-release-${new Date().getTime()}`;
+
+  await createAndUpdateGuidesFiles(gcArticlesGuidesEn, existingContentEN.data.tree, "en", "guides/resources/", branchName);
+  await createAndUpdateGuidesFiles(gcArticlesGuidesFr, existingContentFR.data.tree, "fr", "guides/resources/", branchName);
+
+    let branchcommit = await octokit.request('GET /repos/{owner}/{repo}/commits/{sha}', {
+    owner: 'cds-snc',
+    repo: 'digital-canada-ca',
+    sha: `guides-${branchName}`
+  });
+  let maincommit = await octokit.request('GET /repos/{owner}/{repo}/commits/{sha}', {
+    owner: 'cds-snc',
+    repo: 'digital-canada-ca',
+    sha: "main"
+  })
+  if (branchcommit.data && branchcommit.data.sha != maincommit.data.sha) {
+    closePRs()
+
+    // Make the new PR
+    await octokit.pulls.create({
+      owner: 'cds-snc',
+      repo: 'digital-canada-ca',
+      title: `[AUTO-PR] New content release GUIDES -  ${new Date().toISOString()}`,
+      head: `guides-${branchName}`,
+      base: 'main',
+      body: "New Content release for CDS Website. See below commits for list of changes.",
+      draft: false
+    });
+
+  } else {
+    // no commits, delete the ref
+    await octokit.git.deleteRef({
+      owner: 'cds-snc',
+      repo: 'digital-canada-ca',
+      ref: `heads/guides-${branchName}`
+    });
+  }
+}
 
 async function run() {
   /*
@@ -228,8 +328,8 @@ async function run() {
   var gcArticlesProductSuiteEn = await getProductSuiteFromGCArticles("en");
   var gcArticlesProductSuiteFr = await getProductSuiteFromGCArticles("fr");
   //GC Article Guides
-  var gcArticlesGuidesEn = await getGuidesFromGCArticles("en");
-  var gcArticlesGuidesFr = await getGuidesFromGCArticles("fr");
+  // var gcArticlesGuidesEn = await getGuidesFromGCArticles("en");
+  // var gcArticlesGuidesFr = await getGuidesFromGCArticles("fr");
 
   // Create Ref
   const websiteSha = await getHeadSha("digital-canada-ca", "main");
@@ -281,8 +381,8 @@ async function run() {
   //Guides
   await createAndUpdateFiles(guidesEnNew, existingContentEN.data.tree, "en", "guides/resources/", branchName);
   await createAndUpdateFiles(guidesFrNew, existingContentFR.data.tree, "fr", "guides/resources/", branchName);
-  await createAndUpdateFiles(gcArticlesGuidesEn, existingContentEN.data.tree, "en", "guides/resources/", branchName);
-  await createAndUpdateFiles(gcArticlesGuidesFr, existingContentFR.data.tree, "fr", "guides/resources/", branchName);
+  // await createAndUpdateFiles(gcArticlesGuidesEn, existingContentEN.data.tree, "en", "guides/resources/", branchName);
+  // await createAndUpdateFiles(gcArticlesGuidesFr, existingContentFR.data.tree, "fr", "guides/resources/", branchName);
 
   // if there is content - compare shas of most recent commit on the branch and main
   let branchcommit = await octokit.request('GET /repos/{owner}/{repo}/commits/{sha}', {
@@ -320,3 +420,4 @@ async function run() {
   
 }
 run();
+runGuides();
