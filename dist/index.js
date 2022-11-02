3c0ea5db0db67bb73f124dbf08287b0d84db4d4a
omartehsin1 <>
2022-11-02 09:17:55 -0400

    removed unused code

--- dist/index.js
+++ dist/index.js
@@ -81,22 +81,13 @@ var getBlogPostsFromGCArticles = async function(lang) {
         out += "author: '" + post.meta.gc_author_name + "'\n";
         out += "date: '" + post.date + "'\n";
 
-        out += "image: " + post._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url.replace(
-          "https://cds-website-assets-prod.s3.ca-central-1.amazonaws.com",
-          "https://de2an9clyit2x.cloudfront.net") + "\n";
+        out += "image: " + post._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url + "\n";
         out += "image-alt: " + post._embedded['wp:featuredmedia'][0].alt_text + "\n";
-        out += "thumb: " + post._embedded['wp:featuredmedia'][0].media_details.sizes.large.source_url.replace(
-          "https://cds-website-assets-prod.s3.ca-central-1.amazonaws.com",
-          "https://de2an9clyit2x.cloudfront.net") + "\n";
+        out += "thumb: " + post._embedded['wp:featuredmedia'][0].media_details.sizes.large.source_url + "\n";
         out += "translationKey: " + post.slug + "\n";
         out += "---\n";
 
         // Convert any body image URLS to Cloudfront
-        while (post.markdown.content.rendered.indexOf("https://cds-website-assets-prod.s3.ca-central-1.amazonaws.com") !== -1) {
-          post.markdown.content.rendered = post.markdown.content.rendered.replace(
-            "https://cds-website-assets-prod.s3.ca-central-1.amazonaws.com",
-            "https://de2an9clyit2x.cloudfront.net");
-        }
 
         out += post.content.rendered + "\n";
         
