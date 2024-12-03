5ab3e070b69cd5dce0cede319639e2bdfae15f96
delisma <david@colourfully.digital>
2024-12-03 16:38:48 -0500

    feat: Add categories/blog tags

--- content_fetch/fetch_gc_articles_blog_post.js
+++ content_fetch/fetch_gc_articles_blog_post.js
@@ -1,57 +1,64 @@
-const fetch = require('node-fetch');
-const buildFileName = require("../utils/buildFileName");
+const fetch = require( 'node-fetch' );
+const buildFileName = require( "../utils/buildFileName" );
 
-var getBlogPostsFromGCArticles = async function(lang) {
-  let url = lang == "en" ? process.env.GC_ARTICLES_ENDPOINT_EN + "posts?markdown=true&_embed" : process.env.GC_ARTICLES_ENDPOINT_FR + "posts?markdown=true&_embed"
-  return await fetch(url)
-  .then(response => response.json())
-  .then(
-    data => {
-      let files = [];
-      for (p in data) {
-        let post = data[p]
-        let replacedTitle = post.title.rendered.replace(/&#8217;/g, "’")
-        let category = post._embedded['wp:term']
-        let out = "";
-        out += "---\n";
-        out += "layout: blog\n";
-        out += "title: '" + replacedTitle + "'\n";
-        out += "description: >-\n";
-        out += "  " + post.markdown.excerpt.rendered + "\n";
-        out += "author: '" + post.meta.gc_author_name + "'\n";
-        out += "date: '" + post.date + "'\n";
+const generatePostContent = ( post ) => {
+  const replacedTitle = post.title.rendered.replace( /&#8217;/g, "'" );
+  const category = post._embedded[ 'wp:term' ];
+  
+  let out = '';
+  out += `---\n`;
+  out += `layout: blog\n`;
+  out += `title: '${ replacedTitle }'\n`;
+  out += `description: >-\n  '${ post.markdown.excerpt.rendered }'\n`;
+  out += `author: '${ post.meta.gc_author_name }'\n`;
+  out += `date: '${ post.date }'\n`;
+  out += `image: ${ post._embedded[ 'wp:featuredmedia' ][ 0 ].media_details.sizes.full.source_url }\n`;
+  out += `image-alt: ${ post._embedded[ 'wp:featuredmedia' ][ 0 ].alt_text }\n`;
+  out += `thumb: ${ post._embedded[ 'wp:featuredmedia' ][ 0 ].media_details.sizes.full.source_url }\n`;
 
-        out += "image: " + post._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url + "\n";
-        out += "image-alt: " + post._embedded['wp:featuredmedia'][0].alt_text + "\n";
-        out += "thumb: " + post._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url + "\n";
+  // Process categories
+  if ( category ) {
+    const categoryArray = category[ 0 ].map( cat => `'${ cat.name }'` );
+    out += `category: [ ${ categoryArray } ]\n`;
+  } else {
+    out += `category: [ 'none' ]\n`;
+  }
 
-        // if (category) {
-        //   let innerCategory = category[0]
-        //   let categoryArray = []
+  out += `translationKey: ${ post.slug }\n`;
+  out += `---\n`;
+  out += `${ post.content.rendered }\n`;
 
-        //   for (let i = 0; i < innerCategory.length; i++ ) {
-        //     categoryArray.push(`'${innerCategory[i].name}'`)
-        //   }
+  return out;
+};
 
-        //   out += "category: " + `[${categoryArray}]` + "\n"
-        // } else {
-        //   out += "category: ['none']" + "\n"
-        // }
-        out += "translationKey: " + post.slug + "\n";
-        out += "---\n";
+const getBlogPostsFromGCArticles = async function( lang ) {
+  try {
+    const url = lang === "en" 
+      ? process.env.GC_ARTICLES_ENDPOINT_EN
+      : process.env.GC_ARTICLES_ENDPOINT_FR
 
-        // Convert any body image URLS to Cloudfront
-
-        out += post.content.rendered + "\n";
-        
-        let slug = buildFileName(replacedTitle);
-
-        files.push({body: out, fileName: slug + ".md"})
-      }
-      
-      return files;
+    if ( !url ) {
+      throw new Error(`Missing endpoint configuration for language: ${ lang }`);
     }
-  )
-}
 
+    const response = await fetch( `${ url }posts?markdown=true&_embed` );
+    if ( !response.ok ) {
+      throw new Error( `HTTP error! status: ${ response.status }` );
+    }
+
+    const data = await response.json();
+    if ( !Array.isArray( data ) ) {
+      throw new Error( 'Expected array of posts from API' );
+    }
+
+    return data.map( post => {
+      const content = generatePostContent( post );
+      const fileName = buildFileName( post.title.rendered );
+      return { body: content, fileName: `${ fileName }.md` };
+    } );
+  } catch ( error ) {
+    console.error( 'Failed to fetch blog posts:', error );
+    throw error;
+  }
+}
 module.exports = getBlogPostsFromGCArticles;
